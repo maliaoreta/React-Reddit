a7d02925ea8a3c5c30b35cee796c2572daa3118a
maliaoreta <maliaoreta@gmail.com>
2016-06-02 20:22:22 -1000

    adds broken random subreddit button

--- client/components/App.js
+++ client/components/App.js
@@ -5,9 +5,15 @@ import RedditContent from './RedditContent';
 class App extends React.Component{
   constructor(props) {
     super(props);
+
     this.state = {
-      redditContent: {}
+      redditContent: {},
+      subreddit: ''
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleRandomSubreddit = this.handleRandomSubreddit.bind(this);
   }
   componentWillMount() {
     fetch('https://www.reddit.com/.json')
@@ -21,11 +27,45 @@ class App extends React.Component{
         console.error('error', error); 
       })
   }
+  handleInputChange(event) {
+    this.setState({subreddit: event.target.value})
+  }
+  handleFormSubmit(event) {
+    event.preventDefault();
+
+    fetch(`https://www.reddit.com/r/${this.state.subreddit}.json`)
+      .then((response) => response.text())
+      .then((responseText) => {
+        this.setState({redditContent: JSON.parse(responseText)})
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+  handleRandomSubreddit() {
+    fetch('https://www.reddit.com/r/random')
+      .then((response) => response.text())
+      .then((responseText) => {
+        console.log('responseText: ', responseText);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
   render() {
     return (
       <div className="app">
         <div className="header" style={{fontWeight: 'bold', fontSize: '2em'}}>
           React Reddit
+        </div>
+        <div className="inputSubredditContainer">
+          <form onSubmit={this.handleFormSubmit}>
+            <input onChange={this.handleInputChange} placeholder="View A Subreddit" />
+            <button>Submit</button>
+          </form>
+        </div>
+        <div className="randomSubreddit">
+          <button onClick={this.handleRandomSubreddit}>Random</button>
         </div>
         <RedditContent redditContent={this.state.redditContent} />
       </div>
--- client/components/RedditContent.js
+++ client/components/RedditContent.js
@@ -5,15 +5,19 @@ class RedditContent extends React.Component{
     return this.props.redditContent.data.children.map((eachPost) => {
       if (eachPost.data.preview) {
         return (
-          <div className="post">
-            <div className="postTitle" style={{fontWeight: 'bold'}}>{eachPost.data.title}</div>
-            <img src={eachPost.data.preview.images[0].source.url} style={{height: 500, width: 500}} />
+          <div key={eachPost.data.id} className="post" style={{margin: '1em'}}>
+            <div className="postTitle" style={{fontWeight: 'bold'}}>
+              <a href={eachPost.data.url}>{eachPost.data.title}</a>
+            </div>
+            <img className="postPreview" src={eachPost.data.preview.images[0].source.url} style={{height: 500, width: 500}} />
           </div>
         )
       } else {
         return (
-          <div className="postTitle" style={{fontWeight: 'bold'}}>
-            {eachPost.data.title}
+          <div key={eachPost.data.id} className="post" style={{margin: '1em'}}>
+            <div className="postTitle" style={{fontWeight: 'bold'}}>
+              <a href={eachPost.data.url}>{eachPost.data.title}</a>
+            </div>
           </div>
         )
       }
@@ -22,7 +26,7 @@ class RedditContent extends React.Component{
   render() {
     return (
       <div className="redditContent">
-        {this.getRedditContent()}
+        {this.props.redditContent.data ? this.getRedditContent() : null}
       </div>
     )
   }
