fcd7f63e330463cd7e5ef5463c192722a2c95643
maliaoreta <maliaoreta@gmail.com>
2016-06-04 15:50:22 -1000

    adds MuiThemeProvider for material components

--- client/router.js
+++ client/router.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import App from './components/App';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+injectTapEventPlugin();
+
+const darkMuiTheme = getMuiTheme(darkBaseTheme);
 
 const router = (
-  <Router history = { browserHistory } >
-    <Route path = '/' component = { App } />
-  </Router>
+  <MuiThemeProvider muiTheme={darkMuiTheme}>
+    <Router history = { browserHistory } >
+      <Route path = '/' component = { App } />
+    </Router>
+  </MuiThemeProvider>
 )
 
 ReactDOM.render(
